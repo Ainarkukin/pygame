3d41428f014b58dfb77caca7dd38618a63e9ce67
Ainar Kukin <ainar.kukin@khk.ee>
2021-04-06 09:22:54 +0100

    tegin tasuta

--- game.py
+++ game.py
@@ -5,12 +5,12 @@ def run_game():
     pygame.init()
     screen = pygame.display.set_mode((800, 600))
     pygame.display.set_caption("Example Game")
-
+    bg_color = (222,255,210)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
-
+        screen.fill(bg_color)
         pygame.display.flip()
 
 run_game()
