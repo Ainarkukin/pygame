a51b62e8045117a7cd59b5178faeb3176cf1f0ce
Ainar Kukin <ainar.kukin@khk.ee>
2021-04-06 11:24:40 +0100

    Probleem mille lahendamisega ei saanud hakkama.

--- game.py
+++ game.py
@@ -1,17 +1,20 @@
-import sys
 import pygame
+
+from settings import Settings
+from pablo import Pablo
+import game_functions as gf
 
 def run_game():
     pygame.init()
     game_settings = Settings()
-    screen = pygame.display.set_mode((game_settings.screen_width, game_settings.screen_height))
+    screen = pygame.display.set_mode(game_settings.screen_width, game_settings.screen_height)
     pygame.display.set_caption("Example Game")
-    bg_color = (222,255,210)
+
+
+    pablo = Pablo(screen)
+
     while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-        screen.fill(game_settings.bg_color)
-        pygame.display.flip()
+        gf.check_events()
+        gf.update_screen(game_settings, screen, pablo)
 
 run_game()
