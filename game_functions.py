cb1788703a46749d8beb1b89c915a8e0fcd6e4ac
Ainar Kukin <ainar.kukin@khk.ee>
2021-04-13 06:58:49 +0100

    Probleem mille lahendamisega ei saanud hakkama.

--- game_functions.py
+++ game_functions.py
@@ -1,11 +1,15 @@
 import sys
 import pygame
+from bullet import Bullet
 
-def check_keydown_events(event, ship):
+def check_keydown_events(event, game_settings, screen, ship, bullets):
     if event.key == pygame.K_RIGHT:
         ship.moving_right = True
     if event.key == pygame.K_LEFT:
         ship.moving_left = True
+    if event.key == pygame.K_SPACE:
+        fire_bullet(game_settings, screen, ship, bullets)
+
 
 def check_keyup_events(event, ship):
     if event.key == pygame.K_RIGHT:
@@ -13,17 +17,33 @@ def check_keyup_events(event, ship):
     if event.key == pygame.K_LEFT:
         ship.moving_left = False
 
-def check_events(ship):
+def check_events(game_settings, screen, ship, bullets):
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
         elif event.type == pygame.KEYDOWN:
-            check_keydown_events(event, ship)
+            check_keydown_events(event, game_settings, screen, ship, bullets)
         elif event.type == pygame.KEYUP:
             check_keyup_events(event, ship)
 
 
-def update_screen(game_settings, screen, ship):
+def update_screen(game_settings, screen, ship, bullets):
+    # add screen background
     screen.fill(game_settings.bg_color)
+    for bullet in bullets.sprites():
+        bullet.draw_bullet()
+    # add ship to screen
     ship.blitme()
+    # display the last screen
     pygame.display.flip()
+
+def update_bullets(bullets):
+    bullets.update()
+    for bullet in bullets.copy():
+        if bullet.rect.bottom <= 0:
+            bullets.remove(bullet)
+
+def fire_bullet(game_settings, screen, ship, bullets):
+    if len(bullets) < game_settings.bullets_allowed:
+        new_bullet = Bullet(game_settings, screen, ship)
+        bullets.add(new_bullet)
--- settings.py
+++ settings.py
@@ -1,7 +1,17 @@
 class Settings:
+    """A class to store all setting for example game"""
 
     def __init__(self):
-        self.screen_width = 1000
-        self.screen_height = 900
-        self.bg_color = (225,250,210)
-        self.ship_speed_factor = 0.3
+        """Initialize the game settings"""
+        # screen settings
+        self.screen_width = 800
+        self.screen_height = 600
+        self.bg_color = (238,238,228)
+        # ship settings
+        self.ship_speed_factor = 0.5
+        # bullet settings
+        self.bullet_speed_factor = 0.5
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = 64, 64, 64
+        self.bullets_allowed = 3
